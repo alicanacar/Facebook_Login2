d49713cfa2d088ad4092ce789b58561477887fed
alicanacar <alicanacar@windowslive.com>
2015-11-29 23:00:15 -0800

    Update homeController.js

--- js/modules/app/controllers/homeController.js
+++ js/modules/app/controllers/homeController.js
@@ -14,12 +14,7 @@ var appHomeController = [
         $scope.me = response;
       });
 
-      $scope.fb.q('/me?fields=friends.fields(picture,name)')
-        .then(function (response) {
-          _(response.friends.data).each(function (friend) {
-            $scope.friends.push(friend);
-          });
-        });
+      
     });
   }
 ];
